b266b5dd60d4f5b4fb3ffa3f787ba9613172bbe7
Vishal Sharma <vsharma2473@gmail.com>
2024-05-18 22:57:55 +0530

    Hairsalon project added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,6 +6,7 @@ import skychat from "../../Assets/Projects/skychat.png";
 import foodie from "../../Assets/Projects/foodie.png";
 import player from "../../Assets/Projects/player.png";
 import game from "../../Assets/Projects/game.png";
+import hairsalon from "../../Assets/Projects/hairsalon.png";
 
 
 function Projects() {
@@ -23,7 +24,7 @@ function Projects() {
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={skychat}
-              isBlog={false}
+              isGitHub={true}
               title="SkyChat"
               description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Nodejs. Have features which allows user for realtime messaging, emoji sharing."
               ghLink="https://github.com/vsharma04/realtime-chat-application"
@@ -33,7 +34,7 @@ function Projects() {
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={foodie}
-              isBlog={false}
+              isGitHub={true}
               title="Foodie"
               description="A simple restaurant website template created using react.js and Tailwind Css which takes the content from hardcoded data and renders it using react.js."
               ghLink="https://github.com/vsharma04/food-app"
@@ -43,22 +44,32 @@ function Projects() {
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={player}
-              isBlog={false}
+              isGitHub={true}
               title="Music Player"
               description="A simple music player having play, pause, next and previous button. It shows name and genre of the song and also have a rotating image of the banner of the song. The list of songs are taken from the file containing the json data."
               ghLink="https://github.com/vsharma04/Music_player"
-              demoLink="https://vsharma04.github.io/Music_player/"              
+              demoLink="https://vsharma04.github.io/Music_player/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={game}
-              isBlog={false}
+              isGitHub={true}
               title="Snake Mania"
               description="A simple snake simulation game that reflects score in real time and also shows the high score till reload the page. The snake becomes longer and longer as the game proceeds by engulfing the cubes. The cubes are generating with the help of random function in javascript. All the design and ui is creating with the help of plain HTML and CSS."
               ghLink="https://github.com/vsharma04/Snake_game"
-              demoLink="https://vsharma04.github.io/Snake_game/"              
+              demoLink="https://vsharma04.github.io/Snake_game/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={hairsalon}
+              isGitHub={false}
+              title="HairSalon"
+              description="The HairSalon website is built using the MERN stack, which includes MongoDB, Express.js, React.js, and Node.js, providing a robust, scalable, and efficient platform. This technology stack enables a dynamic and responsive user interface, secure user authentication, and real-time online booking capabilities. The site is hosted on Azure, ensuring high availability and fast performance, with features like a detailed service catalog, responsive design for optimal viewing on all devices. It offers delivering a seamless and secure user experience that supports the salon’s growth."
+              demoLink="https://hairssalon.com/"
             />
           </Col>
 
